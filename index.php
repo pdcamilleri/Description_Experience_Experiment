8165e9ce37eceee8b2d21d5cadbf6c6a1f874bdf
Peter Camilleri <pdc.cse@gmail.com>
2012-11-29 19:14:50 +1100

    The first page

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+$_SESSION['homepage_visit'] = true;
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Expires" content="0" />
+    <meta http-equiv="Cache-Control" content="no-cache" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <title>Decision-making Game</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <!-- script to ensure javascript is enabled -->
+    <script type="text/javascript" src="js/check_js_enabled.js"></script>
+</head>
+<body>
+<div id="container">
+    <p class="emphasize">Instructions</p>
+    <p>
+       Welcome to this decision-making game! 
+       In this task you will be presented with 16 different "choice problems". 
+       Each choice problem will consist of two different options that you must choose between. 
+       Each option will pay out or take away a certain number of points. 
+       Note that some options are stable and will always pay out or take away the <i>same</i> number of points on each play. 
+       Other options are probabilistic and will pay out or take away a <i>different</i> number of points on each play.
+    </p>
+    <p>
+       Your job is to earn the most amount of points by choosing between the options presented in each choice problem. 
+       <b>
+          In some problems you will always lose, so try to lose the least, and in other problems you will always win, so try to win the most
+       </b>. 
+       Please make your decisions as if the points correspond to real money and that each choice is independent from any others.
+    </p>
+    <p>
+       The task will take approximately 16 minutes to complete. 
+       You will also be asked some demographics questions. 
+       <b>
+          Please do not write anything down during the task or use a calculator
+       </b>. 
+       At the end of the task you will receive a code that should be copied back into the Mechanical Turk HIT that will trigger payment. 
+       If you are happy to proceed and are 18 years or older, then click on the 'Play' button below.
+    </p>
+    <form action="Demographics.php" method="get">
+        <div >
+            <button class="myButton" type="submit">Play</button>
+        </div>
+    </form>
+    <!-- Will only appear if javascript id disabled -->
+    <p id="enabled">
+       NOTE: We have checked your browser and found that javascript is not enabled. 
+       You will not be able to complete this task unless you enable javascript from your internet browser options screen.
+    </p>
+</div>
+</body>
+</html>
---
+++ js/check_js_enabled.js
@@ -0,0 +1,5 @@
+//Check that the client browser has javascript enabled
+ window.onload = checkJavaScriptValidity;
+ function checkJavaScriptValidity() {
+     document.getElementById("enabled").innerHTML = ""; 
+ }
