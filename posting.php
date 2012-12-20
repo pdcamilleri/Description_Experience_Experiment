3eca0cf3d09ee556b30c911e9e1974cd71b70648
Peter Camilleri <pdc.cse@gmail.com>
2012-12-20 12:09:24 +1100

    Fixed writing to data folder

--- posting.php
+++ posting.php
@@ -1,7 +1,12 @@
 <?php 
-echo $_POST["age"]; 
+define("DATA_DIR", "data");
+date_default_timezone_set('Australia/Sydney');
 
-$fp = fopen('postingdata.txt', 'a');
+// $filename will be something like this "data/2012-12-20.11:56:04.txt"
+$filename = date("Y-m-d.H:i:s") . ".txt";
+
+//  DIRECTORY_SEPARATOR will be / or \ depending on OS (linux/mac vs Windows)
+$fp = fopen(DATA_DIR . DIRECTORY_SEPARATOR . $filename, 'a') or die("cant open file");
 fwrite($fp, $_POST['name']);
 fwrite($fp, $_POST['time']);
 fclose($fp);
